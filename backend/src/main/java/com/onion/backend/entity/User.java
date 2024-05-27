84e32627b29be79a30b3b8cd6037dc18c0e7cbc9
gnidoc327 <gnidoc327@gmail.com>
2024-05-28 01:05:33 +0900

    Chater 3. Clip 1. JPA 설치 및 설정

---
+++ backend/src/main/java/com/onion/backend/entity/User.java
@@ -0,0 +1,49 @@
+package com.onion.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    private LocalDateTime lastLogin;
+
+    @CreatedDate
+    @Column(insertable = true)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedDate = LocalDateTime.now();
+    }
+}
