cb81d00e220ffe26879a5ff30c3dd91f661ded0a
gnidoc327 <gnidoc327@gmail.com>
2024-09-05 23:00:36 +0900

    Part 6. Chater 1. Clip 5. MySQL MSR 로 재구축

---
+++ docker/mysql-msr/cluster.sql
@@ -0,0 +1,29 @@
+-- 1. Master 서버 SQL
+GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%' IDENTIFIED BY '1q2w3e4r!!';
+FLUSH PRIVILEGES;
+SHOW MASTER STATUS;
+
+-- 2. Slave 서버 SQL
+-- STOP SLAVE;
+-- RESET SLAVE ALL;
+CHANGE MASTER TO
+    MASTER_HOST='mysql-master-a',
+    MASTER_USER='replication_user',
+    MASTER_PASSWORD='1q2w3e4r!!',
+    MASTER_LOG_FILE='mysql-bin.000003',
+    MASTER_LOG_POS=654
+FOR CHANNEL 'master_a_channel';
+START SLAVE FOR CHANNEL 'master_a_channel';
+CHANGE MASTER TO
+    MASTER_HOST='mysql-master-b',
+    MASTER_USER='replication_user',
+    MASTER_PASSWORD='1q2w3e4r!!',
+    MASTER_LOG_FILE='mysql-bin.000003',
+    MASTER_LOG_POS=654
+    FOR CHANNEL 'master_b_channel';
+START SLAVE FOR CHANNEL 'master_b_channel';
+SHOW SLAVE STATUS;
+
+SHOW TABLES;
+
+-- SET GLOBAL read_only = OFF;
