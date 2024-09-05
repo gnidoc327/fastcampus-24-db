8fad4f22572d23335137fdde682cc167583ee0c7
gnidoc327 <gnidoc327@gmail.com>
2024-09-05 22:18:41 +0900

    Part 6. Chater 1. Clip 2. MySQL Master/Slave Replication 로 재구축

---
+++ docker/mysql-replication/cluster.sql
@@ -0,0 +1,21 @@
+-- 1. Master 서버 SQL
+GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%' IDENTIFIED BY '1q2w3e4r!!';
+FLUSH PRIVILEGES;
+SHOW MASTER STATUS;
+
+-- 2. Slave 서버 SQL
+-- STOP SLAVE;
+-- RESET SLAVE ALL;
+CHANGE MASTER TO
+    MASTER_HOST='mysql-master',
+    MASTER_USER='replication_user',
+    MASTER_PASSWORD='1q2w3e4r!!',
+    MASTER_LOG_FILE='<TODO>',
+    MASTER_LOG_POS=<TODO>;
+START SLAVE;
+
+SHOW SLAVE STATUS;
+
+SHOW TABLES;
+
+# SET GLOBAL read_only = OFF;
